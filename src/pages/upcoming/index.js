b28aba76a09256dec89d98802716e5bac40600ff
Rupesh Raj <rupeshr053@gmail.com>
2022-07-01 22:06:49 +0530

    typo

--- src/pages/upcoming/index.js
+++ src/pages/upcoming/index.js
@@ -12,13 +12,13 @@ export default function (props) {
                 <MatchLink />
             </div>
 
-            <div id="carouselExampleIndicators" class="carousel slide relative" data-bs-ride="carousel">
-                <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+            <div id="carouselExampleIndicators" className="carousel slide relative" data-bs-ride="carousel">
+                <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
                     <button
                         type="button"
                         data-bs-target="#carouselExampleIndicators"
                         data-bs-slide-to="0"
-                        class="active"
+                        className="active"
                         aria-current="true"
                         aria-label="Slide 1"
                     ></button>
@@ -35,30 +35,30 @@ export default function (props) {
                         aria-label="Slide 3"
                     ></button>
                 </div>
-                <div class="carousel-inner relative w-full overflow-hidden">
+                <div className="carousel-inner relative w-full overflow-hidden">
                     {props.schedules && props.schedules.length && props.schedules.forEach((schedule, index) => {
-                        <div class="carousel-item active float-left w-full" key={index}>
+                        <div className="carousel-item active float-left w-full" key={index}>
                             <Card schedule={schedule} />
                         </div>
                     })}
                 </div>
                 <button
-                    class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                    className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide="prev"
                 >
-                    <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
+                    <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
                 </button>
                 <button
-                    class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                    className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide="next"
                 >
-                    <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
+                    <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
                 </button>
             </div>
             <div className="my-4">
